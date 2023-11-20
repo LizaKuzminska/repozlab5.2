3bd010beed4633f46490031482a96462d421f28c
LizaKuzminska <yelyzavetakuzminska@gmail.com>
2023-11-20 19:03:24 +0200

    dbgd

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double z;
+			z = A(0.5, 1, 12.0);
+			Assert::AreEqual(z, 1.0);
+		}
+	};
+}
